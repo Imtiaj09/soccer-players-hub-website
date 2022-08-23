304d4079fd892b03b2c8b0587561c59106738252
Imtiaj09 <ahmimtiaj@gmail.com>
2022-08-23 23:41:41 +0600

    players manager coach total budget added

--- js/common.js
+++ js/common.js
@@ -1,1 +1,25 @@
-console.log("click")
+function getValueofField(fieldId) {
+  const fieldValue = parseInt(document.getElementById(fieldId).value);
+  return fieldValue;
+}
+function setValueofElelment(elementId, value) {
+  const elelement = document.getElementById(elementId);
+  elelement.innerText = value;
+}
+
+//
+document.getElementById('btn-calculate').addEventListener('click', function () {
+  const playerNumber = document.getElementById('player-list').getElementsByTagName('li').length;
+  const newCalculateAmount = getValueofField('calculate-field');
+  const newExpenceTotal = newCalculateAmount * playerNumber;
+  setValueofElelment('expence-total', newExpenceTotal);
+})
+
+document.getElementById('btn-total').addEventListener('click', function () {
+  const newExpenceTotalString = document.getElementById('expence-total').innerText;
+  const newExpenceTotal = parseInt(newExpenceTotalString);
+  const managerTotalAmount = getValueofField('manager-field');
+  const coachTotalAmount = getValueofField('coach-field');
+  const newCalculateAmmount = newExpenceTotal + managerTotalAmount + coachTotalAmount;
+  setValueofElelment('total-calculation', newCalculateAmmount);
+})
