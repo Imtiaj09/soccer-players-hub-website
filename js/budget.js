0bc63bdcbc3c784f41adc321f9119b0ee8afcef0
Imtiaj09 <ahmimtiaj@gmail.com>
2022-08-23 23:45:08 +0600

    card picture added and something thange added

--- js/budget.js
+++ js/budget.js
@@ -16,3 +16,22 @@ for (const button of buttons) {
     }
   })
 }
+
+
+//Players-Manager-Coach total Budget
+
+document.getElementById('btn-calculate').addEventListener('click', function () {
+  const playerNumber = document.getElementById('player-list').getElementsByTagName('li').length;
+  const newCalculateAmount = getValueofField('calculate-field');
+  const newExpenceTotal = newCalculateAmount * playerNumber;
+  setValueofElelment('expence-total', newExpenceTotal);
+})
+
+document.getElementById('btn-total').addEventListener('click', function () {
+  const newExpenceTotalString = document.getElementById('expence-total').innerText;
+  const newExpenceTotal = parseInt(newExpenceTotalString);
+  const managerTotalAmount = getValueofField('manager-field');
+  const coachTotalAmount = getValueofField('coach-field');
+  const newCalculateAmmount = newExpenceTotal + managerTotalAmount + coachTotalAmount;
+  setValueofElelment('total-calculation', newCalculateAmmount);
+})
--- js/common.js
+++ js/common.js
@@ -6,20 +6,3 @@ function setValueofElelment(elementId, value) {
   const elelement = document.getElementById(elementId);
   elelement.innerText = value;
 }
-
-//
-document.getElementById('btn-calculate').addEventListener('click', function () {
-  const playerNumber = document.getElementById('player-list').getElementsByTagName('li').length;
-  const newCalculateAmount = getValueofField('calculate-field');
-  const newExpenceTotal = newCalculateAmount * playerNumber;
-  setValueofElelment('expence-total', newExpenceTotal);
-})
-
-document.getElementById('btn-total').addEventListener('click', function () {
-  const newExpenceTotalString = document.getElementById('expence-total').innerText;
-  const newExpenceTotal = parseInt(newExpenceTotalString);
-  const managerTotalAmount = getValueofField('manager-field');
-  const coachTotalAmount = getValueofField('coach-field');
-  const newCalculateAmmount = newExpenceTotal + managerTotalAmount + coachTotalAmount;
-  setValueofElelment('total-calculation', newCalculateAmmount);
-})
