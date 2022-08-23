c844dc4b6531fa49d0458634177e6931c78a21d2
Imtiaj09 <ahmimtiaj@gmail.com>
2022-08-23 23:13:36 +0600

    total player section added

---
+++ js/budget.js
@@ -0,0 +1,18 @@
+//
+const buttons = document.getElementsByClassName('select-button');
+for (const button of buttons) {
+  button.addEventListener('click', function (event) {
+    const playerName = (event.target.parentNode.childNodes[1].innerText);
+    const playerList = document.getElementById('player-list'); //ol
+    const listlength = playerList.getElementsByTagName('li').length;
+    if (listlength >= 5) {
+      alert('Maximum five players can be slected');
+    }
+    else {
+      const element = document.createElement('li');
+      element.innerText = playerName;
+      playerList.appendChild(element);
+      event.target.setAttribute('disabled', true)
+    }
+  })
+}
